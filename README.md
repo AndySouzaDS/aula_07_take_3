58d28d34aa2a21b5bf243f15313f42c762d05b1e
andysouza <andysouzads@gmail.com>
2024-05-17 22:55:40 -0300

    salvando a primeira função

--- README.md
+++ README.md
@@ -1,3 +1,2 @@
 # aula_07_take_3
-# aula_07_take_3
-# aula_07_take_3
+
---
+++ exemplo_00.py
@@ -0,0 +1,14 @@
+def soma(valor_1_para_somar: float, valor_2_para_somar: float) -> float:
+    """Função de Soma
+
+    Args:
+        valor_1_para_somar (float): valor simples do tipo float
+        valor_2_para_somar (float): valor simples do tipo float
+
+    Returns:
+        float: retorna a soma de dois valores float
+    """
+    resultado_da_soma = valor_1_para_somar + valor_2_para_somar
+    return resultado_da_soma
+
+print(soma(4, 6))
